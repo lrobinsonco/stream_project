21500c37467ff99a86ba84527cdb6a2960ffe66e
lrobinsonco <lrobinsonco@gmail.com>
2017-03-14 22:42:25 -0600

    set element

--- js/API.js
+++ js/API.js
@@ -133,6 +133,8 @@ function init() {
 
 var clearCreek = {clearCreek: "https://waterservices.usgs.gov/nwis/iv/?format=json&indent=on&sites=06716500&parameterCd=00060&siteType=ST&siteStatus=all", clearGraph: "http://waterdata.usgs.gov/nwisweb/graph?agency_cd=USGS&site_no=06716500&parm_cd=00060&period=7", clearMap: (39.765833, -105.625556), clearLocal: "Clear Creek at Lawson, CO"};
 
+console.log(clearCreek);
+
 var deckers = {deckers: "https://waterservices.usgs.gov/nwis/iv/?format=json&indent=on&sites=06701900&parameterCd=00060&siteType=ST&siteStatus=all", deckersGraph: "https://waterdata.usgs.gov/nwisweb/graph?agency_cd=USGS&site_no=06701900&parm_cd=00060&period=7", deckersMap: (39.26, -105.221389), deckersLocal: "South Platte near Trumbull, CO"};
 
 
@@ -141,26 +143,32 @@ var thompson = {thompson: "https://waterservices.usgs.gov/nwis/iv/?format=json&i
 
 var bear = {bear: "https://waterservices.usgs.gov/nwis/iv/?format=json&indent=on&sites=06710605&parameterCd=00060&siteType=ST&siteStatus=all", bearGraph: "https://waterdata.usgs.gov/nwisweb/graph?agency_cd=USGS&site_no=06710605&parm_cd=00060&period=7", bearMap: (39.651944, -105.173056), bearLocal: "Bear creek at Morrison, CO"};
 
-
-
 // var waterData = "https://data.colorado.gov/resource/a97x-8zfv.json";
+
 $(document).ready(function() {
     console.log("ready");
-    $('.dropdown-menu li').click(function() {
+    //select element
+    $('.dropdown-menu li').click(function(event) {
       var clickSelect =  $(this).attr("data-id");
         console.log(clickSelect);
-        console.log('click');
+// set that element to the variable above.
+        if(clickSelect === "bearCreek") {
+          $(".flow_title").val("Bear creek at Morrison, CO");
+        }
+
+
+// append the information to the DOM
     });
 
-    $.get(flow).then(function(data) {
-
-        var flow = data.value.timeSeries[0].values[0].value[0].value;
-        console.log(flow);
-        showFlow(flow);
-        console.log(data);
-    });
-
-    function showFlow(flow) {
-        $(".flow_rate").html('<span class="poop">' + flow + '</span>');
-    }
+    // $.get().then(function(data) {
+    //
+    //     var flow = data.value.timeSeries[0].values[0].value[0].value;
+    //     console.log(flow);
+    //     showFlow(flow);
+    //     console.log(data);
+    // });
+    //
+    // function showFlow(flow) {
+    //     $(".flow_rate").html('<span class="poop">' + flow + '</span>');
+    // }
 });
